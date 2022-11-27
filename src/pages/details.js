193bd2affce593b754e85b021b0f44ae54940b2f
Shirley <illangayo00@gmail.com>
2022-11-27 22:29:41 +0900

    class활용한 라이프사이클 훅

--- src/pages/details.js
+++ src/pages/details.js
@@ -1,6 +1,18 @@
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
 
+class Detail2 extends React.Component {
+  componentDidMount() {
+    // 컴포넌트 mount시 실행 코드
+  }
+  componentDidUpdate() {
+    // 컴포넌트 update시 실행코드
+  }
+  componentWillUnmount() {
+    // 컴포넌트 unmount시 실행코드
+  }
+}
+
 function Detail({ shoes }) {
   let { num } = useParams();
   let theProduct = shoes.find((shoe) => shoe.id === num - 1);
