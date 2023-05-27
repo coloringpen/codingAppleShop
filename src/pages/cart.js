7b94e3803c89f0fc7abbd8eb69a5d5b1a94702ed
coloringpen <illangayo00@gmail.com>
2023-05-27 17:52:29 +0900

    userSlice.js 확인

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,59 +1,51 @@
-import Table from 'react-bootstrap/Table';
-import { useDispatch, useSelector } from 'react-redux';
-import { changeName, changeItemNum, changeAge } from '../store';
+import Table from "react-bootstrap/Table";
+import { useDispatch, useSelector } from "react-redux";
+import { changeItemNum } from "../store";
 
 function Cart() {
-  let theStates = useSelector((state) => {
-    return state;
-  }); // Redux store에 있던 state 남음
-  let { cartContents } = theStates;
-  let dispatch = useDispatch();
+	let theStates = useSelector((state) => {
+		return state;
+	}); // Redux store에 있던 state 남음
+	let { cartContents } = theStates;
+	let dispatch = useDispatch();
 
-  return (
-    <>
-      <div>{theStates.user.name}'s cart</div>
-      <div>kim's age is {theStates.user.age}</div>
-      <button
-        onClick={() => {
-          dispatch(changeAge());
-        }}
-      >
-        age +1
-      </button>
-      <div>
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>product</th>
-              <th>amount</th>
-              <th>change</th>
-            </tr>
-          </thead>
-          <tbody>
-            {cartContents.map((item, index) => {
-              return (
-                <tr key={index}>
-                  <td>{item.id}</td>
-                  <td>{item.name}</td>
-                  <td>{item.count}</td>
-                  <td>
-                    <button
-                      onClick={() => {
-                        dispatch(changeItemNum(item.id));
-                      }}
-                    >
-                      +
-                    </button>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </Table>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div>{theStates.user.name}'s cart</div>
+			<div>
+				<Table striped bordered hover>
+					<thead>
+						<tr>
+							<th>#</th>
+							<th>product</th>
+							<th>amount</th>
+							<th>change</th>
+						</tr>
+					</thead>
+					<tbody>
+						{cartContents.map((item, index) => {
+							return (
+								<tr key={index}>
+									<td>{item.id}</td>
+									<td>{item.name}</td>
+									<td>{item.count}</td>
+									<td>
+										<button
+											onClick={() => {
+												dispatch(changeItemNum(item.id));
+											}}
+										>
+											+
+										</button>
+									</td>
+								</tr>
+							);
+						})}
+					</tbody>
+				</Table>
+			</div>
+		</>
+	);
 }
 
 export default Cart;
--- src/store.js
+++ src/store.js
@@ -1,58 +1,58 @@
-import { configureStore, createSlice } from '@reduxjs/toolkit';
-import user from './store/userSlice.js'; // 슬라이스 파일 임포트로 분할가능
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+import user from "./store/userSlice.js"; // 슬라이스 파일 임포트로 분할가능
 
 let stock = createSlice({
-  name: 'stock',
-  initialState: [10, 11, 12],
+	name: "stock",
+	initialState: [10, 11, 12],
 });
 
 /** 장바구니 들어있는 갯수 변경 */
 let cartContents = createSlice({
-  name: 'cart',
-  initialState: [
-    { id: 0, name: 'White and Black', count: 2 },
-    { id: 2, name: 'Grey Yordan', count: 1 },
-  ],
-  reducers: {
-    changeItemNum(state, id) {
-      let newState = state.map((i) => {
-        if (i.id === id.payload) {
-          i.count += 1;
-          return i;
-        } else {
-          return i;
-        }
-      });
-      state = newState;
-    },
-    addItems(state, action) {
-      let newState = null;
-      let idArray = state.map((i) => i.id);
-      // if (idArray.find((i) => i === action.payload.id)) {
-      // find로 하면 id가 0인 친구의 경우, 반환값이 falsy돼서 안됨
-      if (idArray.includes(action.payload.id)) {
-        newState = state.map((i) => {
-          if (i.id === action.payload.id) {
-            i.count += 1;
-            return i;
-          } else {
-            return i;
-          }
-        });
-        state = newState;
-      } else {
-        state.push(action.payload);
-      }
-    },
-  },
+	name: "cart",
+	initialState: [
+		{ id: 0, name: "White and Black", count: 2 },
+		{ id: 2, name: "Grey Yordan", count: 1 },
+	],
+	reducers: {
+		changeItemNum(state, id) {
+			let newState = state.map((i) => {
+				if (i.id === id.payload) {
+					i.count += 1;
+					return i;
+				} else {
+					return i;
+				}
+			});
+			state = newState;
+		},
+		addItems(state, action) {
+			let newState = null;
+			let idArray = state.map((i) => i.id);
+			// if (idArray.find((i) => i === action.payload.id)) {
+			// find로 하면 id가 0인 친구의 경우, 반환값이 falsy돼서 안됨
+			if (idArray.includes(action.payload.id)) {
+				newState = state.map((i) => {
+					if (i.id === action.payload.id) {
+						i.count += 1;
+						return i;
+					} else {
+						return i;
+					}
+				});
+				state = newState;
+			} else {
+				state.push(action.payload);
+			}
+		},
+	},
 });
 
 export let { changeItemNum, addItems } = cartContents.actions;
 
 export default configureStore({
-  reducer: {
-    user: user.reducer,
-    stock: stock.reducer,
-    cartContents: cartContents.reducer,
-  },
+	reducer: {
+		user: user.reducer,
+		stock: stock.reducer,
+		cartContents: cartContents.reducer,
+	},
 });
